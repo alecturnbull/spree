1b99a1f418307372f9d1b7e3403a9cbc9f3398a4
Alec Turnbull <alec.turnbull@gmail.com>
2013-02-19 13:31:45 -0500

    Spree now serves static assets

--- core/app/controllers/content_controller.rb
+++ core/app/controllers/content_controller.rb
@@ -1,6 +1,7 @@
 class ContentController < Spree::BaseController
+  # OVERRIDING THIS FOR BETTER DEVELOPMENT
   # Don't serve local files or static assets
-  before_filter { render_404 if params[:path] =~ /(\.|\\)/ }
+  # before_filter { render_404 if params[:path] =~ /(\.|\\)/ }
 
   rescue_from ActionView::MissingTemplate, :with => :render_404
   caches_page :show, :index, :if => Proc.new { Spree::Config[:cache_static_content] }
